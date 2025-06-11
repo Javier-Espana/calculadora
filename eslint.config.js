c17ffe48e76cafe34758dbb86b7e907bb98037dd
Javier-Espana <javier.eduardo.espana@gmail.com>
2025-06-10 20:09:45 -0600

    Fixed ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,25 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  js.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
       },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'semi': ['error', 'never'],
+      'max-len': ['error', { code: 120 }],
+      'react/react-in-jsx-scope': 'off',
     },
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '*.config.js',
+    ],
   },
 ]
